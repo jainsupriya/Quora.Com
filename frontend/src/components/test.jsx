a17441d41c469a84cdf21b675efdc20da2a55b01
jainsupriya <supriya.jain@sjsu.edu>
2019-05-03 14:44:23 -0700

    fixed issue in home.jsx

---
+++ frontend/src/components/test.jsx
@@ -0,0 +1,15 @@
+import React,{Component} from 'react'
+import Editor from "../components/Quora/AnswerComponent/Editor";
+
+class Test extends Component{
+    render()
+    {
+        return(
+        <div >  
+            <Editor/>
+        </div>
+        )
+    }
+};
+
+export default Test;
