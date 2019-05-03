d733049f67d99a0926ce9b75848538a7948af306
Namrata <namratakasar25@gmail.com>
2019-05-03 16:48:37 -0700

    adding sample pie chart

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -9,6 +9,7 @@ import Quora from "./Quora/Quora";
 import Content from "./Quora/Content";
 import Profile from "./Quora/profile/Profile";
 import PrivateRoute from "./auth/PrivateRoute";
+import Dashboard from "./Quora/Dashboard";
 
 // Check for token
 // if (localStorage.jwtToken) {
@@ -41,7 +42,8 @@ class Main extends Component {
               <PrivateRoute exact path="/" component={Quora} />
               <Route path="/signup" component={SignUp} />
               <Route path="/login" component={Login} />
-              <Route path="/content" component={Content} />
+              <Route path="/content" component={Content} />              
+              <Route path="/dashboard" component={Dashboard} />
               <Route path="/profile" component={Profile} />
               <PrivateRoute path="/home" component={Quora} />
               <Route render={() => <h3>Page not Found</h3>} />
