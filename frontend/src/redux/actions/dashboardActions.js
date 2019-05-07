32e66c16ae18119952b5c48256859a04ba7f7227
Namrata <namratakasar25@gmail.com>
2019-05-07 02:18:08 -0700

    changing dashboard charts

--- frontend/src/redux/actions/dashboardActions.js
+++ frontend/src/redux/actions/dashboardActions.js
@@ -2,12 +2,13 @@ import axios from "axios";
 import {
     GET_ANSWER_VIEWS,
     GET_ANSWER_UPVOTES,
+    GET_ANSWER_DOWNVOTES,
     GET_ERRORS
 } from "./types";
 
-export const getAnswersByViews  = () => dispatch => {
+export const getAnswersByViews  = userId => dispatch => {
   axios
-    .get(`/answers/orderByViews`)
+    .get(`/answers/orderByViews/${userId}`)
     .then(res => {
         console.log(res);
         dispatch({
@@ -25,13 +26,33 @@ export const getAnswersByViews  = () => dispatch => {
     );
 };
 
-export const getAnswersByUpvotes  = () => dispatch => {
+export const getAnswersByUpvotes  = userId => dispatch => {
   axios
-    .get(`/answers/orderByUpVotes`)
+    .get(`/answers/orderByUpVotes/${userId}`)
     .then(res => {
         console.log(res);
         dispatch({
             type: GET_ANSWER_UPVOTES,
+            payload: res.data
+          })
+        }      
+    )
+    .catch(err =>
+        console.log(err)
+    //   dispatch({
+    //     type: GET_ERRORS,
+    //     payload: err.data
+    //   })
+    );
+};
+
+export const getAnswersByDownvotes  = userId => dispatch => {
+  axios
+    .get(`/answers/orderByDownVotes/${userId}`)
+    .then(res => {
+        console.log(res);
+        dispatch({
+            type: GET_ANSWER_DOWNVOTES,
             payload: res.data
           })
         }      
